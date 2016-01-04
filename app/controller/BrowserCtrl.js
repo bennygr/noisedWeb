e660ac373b1a1d2f125fd58fd96f039a3932616d
Benny <bennygr@gmx.net>
2016-01-04 19:26:24 +0100

    Executing search command

--- app/controller/BrowserCtrl.js
+++ app/controller/BrowserCtrl.js
@@ -2,24 +2,27 @@ noisedWeb.controller('BrowserCtrl', function($scope,
 											 ConnectionSettingsStorage,
 											 ConnectionManager,
 											 Command){
+	$scope.searchInput = null;
+	$scope.searchValue = null;
+	$scope.searchResult = null;
 
 	$scope.search = function(){
-		var person = prompt("Please enter a search pattern", "Harry Potter");
 		var connection = ConnectionManager.getCurrentConnection();
 		if(connection){
 			var command = 
 					{ 
 						'Name': 'Noised.Plugins.Commands.CoreCommands.Search',
-						'Parameters': [person]
+						'Parameters': [$scope.searchInput]
 					};  
 			Command.sendCommand(connection,command);
 		}
 	}
 	
 	var searchResultHandler = function(connection, response){
-		var p = response.Parameters[0][0].MediaItems;
-		alert(p.length + " results found");
-		alert(JSON.stringify(p));
+		var resultList = response.Parameters[0][0].MediaItems;
+		$scope.searchResult = resultList;
+		$scope.searchValue = $scope.searchInput;
+		$scope.$digest();
 	}
 
 	Command.registerResponseCallback(searchResultHandler,/Noised.\Commands\.Core\.Search/);

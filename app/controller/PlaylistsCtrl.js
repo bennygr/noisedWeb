5e2d32de2af121cb7fd87026174373a31a11d633
Benny <bennygr@gmx.net>
2016-02-20 08:49:38 +0100

    More playlist handling

--- app/controller/PlaylistsCtrl.js
+++ app/controller/PlaylistsCtrl.js
@@ -1,24 +1,24 @@
 noisedWeb.controller('PlaylistsCtrl', function($scope,Playlists){
-	Playlists.refresh();
-	$scope.playlists = Playlists.playlists;
-	$scope.selectedPlaylist = null;
+    Playlists.refresh();
+    $scope.playlists = Playlists.playlists;
+    $scope.selectedPlaylist = null;
 
-	$scope.$watch(
-		function(){return Playlists.playlists},
-		function(newVal){
-			$scope.playlists = newVal;
-		}
-	);
+    $scope.$watch(
+        function(){ return Playlists.playlists},
 
-	$scope.createPlaylist = function(){
-		Playlists.createPlaylist("123" + new Date().getSeconds());
-	}
-	$scope.deletePlaylist = function(){
-		Playlists.deletePlaylist("123");
-	}
+        function(newVal){
+            $scope.playlists = newVal;
+        });
 
-	$scope.selectPlaylist = function(playlist){
-		$scope.selectedPlaylist = playlist;
-		alert(JSON.stringify(playlist));
-	}
+    $scope.createPlaylist = function(){
+        Playlists.createPlaylist("123" + new Date().getSeconds());
+    }
+
+    $scope.deletePlaylist = function(){
+        Playlists.deletePlaylist("123");
+    }
+
+    $scope.selectPlaylist = function(playlist){
+        $scope.selectedPlaylist = playlist;
+    }
 });
--- app/services/Media/PlaylistsService.js
+++ app/services/Media/PlaylistsService.js
@@ -1,56 +1,53 @@
 //handles the noised queue
 noisedWeb.service('Playlists',function($rootScope,ConnectionManager,Command){
-	var _this = this;
-	this.playlists = [];
+    var _this = this;
+    this.playlists = [];
 
-	this.triggerUpdate = function(){
-		$rootScope.$digest()
-	};
+    this.triggerUpdate = function(){
+        $rootScope.$digest()
+    };
 
-	this.refresh = function(){
-		var command = 
-				{ 
-                                    'Name': 'Noised.Plugins.Commands.CoreCommands.GetPlaylists'
-				};  
-		ConnectionManager.sendCommand(command);
-	};
+    this.refresh = function(){
+        var command = { 
+            'Name': 'Noised.Plugins.Commands.CoreCommands.GetPlaylists'
+        };  
+        ConnectionManager.sendCommand(command);
+    };
 
-	this.createPlaylist = function(name){
-		var command = 
-				{ 
-					'Name': 'Noised.Plugins.Commands.CoreCommands.CreatePlaylist',
-					'Parameters': [name]
-				};  
-		ConnectionManager.sendCommand(command);
-	}
+    this.createPlaylist = function(name){
+        var command = { 
+            'Name': 'Noised.Plugins.Commands.CoreCommands.CreatePlaylist',
+            'Parameters': [name]
+        };  
+        ConnectionManager.sendCommand(command);
+    }
 
-	this.deletePlaylist = function(name){
-		var command = 
-				{ 
-					'Name': 'Noised.Plugins.Commands.CoreCommands.DeletePlaylist',
-					'Parameters': [name]
-				};  
-		ConnectionManager.sendCommand(command);
-	}
+    this.deletePlaylist = function(name){
+        var command = { 
+            'Name': 'Noised.Plugins.Commands.CoreCommands.DeletePlaylist',
+            'Parameters': [name]
+        };  
+        ConnectionManager.sendCommand(command);
+    }
 
-        this.addToPlaylist = function(playlist, item){
-		var command = { 
-                    'Name': 'Noised.Plugins.Commands.CoreCommands.AddToPlaylist',
-                    'Parameters': [playlist.Name,[item.Uri]]
-                };  
-                ConnectionManager.sendCommand(command);
+    this.addToPlaylist = function(playlist, item){
+        var command = { 
+            'Name': 'Noised.Plugins.Commands.CoreCommands.AddToPlaylist',
+            'Parameters': [playlist.Name,[item.Uri]]
+        };  
+        ConnectionManager.sendCommand(command);
+    }
+
+    this.refreshHandler = function(connection, response){
+        if(response.Parameters.length > 0) {
+            _this.playlists = response.Parameters;
         }
+        else {
+            _this.playlists = null;
+        }
+        _this.triggerUpdate();
+    };
 
-	this.refreshHandler = function(connection, response){
-		if(response.Parameters.length > 0) {
-			_this.playlists = response.Parameters;
-		}
-		else{
-			_this.playlists = null;
-		}
-		_this.triggerUpdate();
-	};
-
-	Command.registerResponseCallback(this.refreshHandler,/Noised.\Commands\.Core\.GetPlaylists/);
-	this.refresh();
+    Command.registerResponseCallback(this.refreshHandler,/Noised.\Commands\.Core\.GetPlaylists/);
+    this.refresh();
 })

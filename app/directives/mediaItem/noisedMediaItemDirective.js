0860a732ffcd4dcaf609c92ca21c5a785587caca
bennygr <bennygr@gmx.net>
2016-05-25 17:16:46 +0200

    angular test directive

---
+++ app/directives/mediaItem/noisedMediaItemDirective.js
@@ -0,0 +1,9 @@
+/**
+ * A directive for displaying mediaItems
+ */
+noisedWeb.directive('noisedMediaItem',function(){
+    return{
+        restricted: 'E',
+        templateUrl: 'app/directives/mediaItem/noisedMediaItemDirective.html'
+    };
+});
